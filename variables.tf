09d161a1a5939b77b899e7d6268d95fe7efffc9f
konef <40858004+konef@users.noreply.github.com>
2024-08-22 23:26:48 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,3 @@
-variable "subscription_id" {
-  description = "The ID of the subscription where the resource group will be created"
-  type        = string
-}
-
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
