f38091974b9b7234c43efce1932ce10cbfd89ce2
konef <40858004+konef@users.noreply.github.com>
2024-08-22 23:25:56 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-provider "azurerm" {
-  features = {}
-  subscription_id = var.subscription_id
-}
-
 resource "azurerm_resource_group" "this" {
   name     = var.resource_group_name
   location = var.location
